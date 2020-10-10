0807d71e3e71118e1c0d42968aa48e5c61221dd9
KAY S <KS.KWANRUAN@GMAIL.com>
2020-10-10 14:29:16 +1100

    rename route

--- routes/staff.js
+++ routes/staff.js
@@ -5,7 +5,7 @@ const { create, list, read, remove, update } = require('../controllers/staff');
 const { requireSignin, adminMiddleware } = require('../controllers/auth');
 
 router.post('/staff', requireSignin, adminMiddleware, create);
-router.get('/staff', list);
+router.get('/allstaff', list);
 router.get('/staff/:slug', read);
 router.delete('/staff/:slug', requireSignin, adminMiddleware, remove);
 router.put('/staff/:slug', requireSignin, adminMiddleware, update);
