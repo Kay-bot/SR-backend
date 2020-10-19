052d37fc2f1c19254b8da6380ff34fc08ea83d53
KAY S <KS.KWANRUAN@GMAIL.com>
2020-10-20 10:17:27 +1100

    contact form

---
+++ controllers/form.js
@@ -0,0 +1,27 @@
+const { sendEmailWithNodemailer } = require('../helpers/email');
+
+exports.contactForm = (req, res) => {
+  const { email, name, message } = req.body;
+
+  const emailData = {
+    to: process.env.EMAIL_TO,
+    from: email,
+    subject: `Contact from - ${process.env.APP_NAME}`,
+    text: `
+    Email received from contact from
+    Sender name: ${name}
+    Sender email: ${email}
+    Sender message:${message}`,
+    html: `
+    <h4>Email received from contact form</h4>
+    <p>Sender name: ${name}</p>
+    <p>Sender email: ${email}</p>
+    <p>Sender message: ${message}</p>
+    <hr />
+    <p>This email may contain sensitive information</p>
+    <p>https://strataroofing.com.au</p>
+    `,
+  };
+
+  sendEmailWithNodemailer(req, res, emailData);
+};
